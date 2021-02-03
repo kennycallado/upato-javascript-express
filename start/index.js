5071b4a2ff5df88e4d6c9c0406fb6df7def8a14a
kennycallado <kennycallado@hotmail.com>
2021-02-03 19:48:19 +0100

    Incluye typeorm. - Modifica start/index.js para incluir typeorm - Modifica .env cambiadas algunas variables. Esto genera una conexion con base de datos (sqlite)

--- start/index.js
+++ start/index.js
@@ -1,6 +1,8 @@
 // Librerías requeridas
 const Express = require('express');
 const morgan = require('morgan');
+const { createConnection } = require('typeorm');
+require('reflect-metadata');
 
 // Módulos de la aplicación requeridos
 const userRouter = require('./routes/UserRouter.js');
@@ -10,13 +12,14 @@ const port = process.env.PORT || 3000;
 
 // Inicia la aplicación
 const app = new Express();
+createConnection(); //Simplemente se ejecuta ya que coge datos de variables del sistema
 
 // Middleware
 app.use(morgan('tiny'));
 app.use(Express.json());
 
 // Routes
-app.use('/users',userRouter);
+app.use('/users', userRouter);
 
 // Listening
 app.listen(port);
