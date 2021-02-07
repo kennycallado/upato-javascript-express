21b272c825894d173400e063432e743cb439e297
kennycallado <kennycallado@hotmail.com>
2021-02-07 15:16:30 +0100

    Implementa relaciones.

--- app/Controllers/CommentController.js
+++ app/Controllers/CommentController.js
@@ -1,50 +1,74 @@
 const { getRepository } = require('typeorm');
 
 const Comment = require('../Models/Comment');
+const User = require('../Models/User');
+const Post = require('../Models/Post');
 
 class CommentController {
   async index(req, res) {
-    const comments = await getRepository('Comment').find();
+    const comments = await getRepository(Comment).find();
+
+    for (const comment of comments) {
+      const post = await getRepository(Post).findOne(comment.post);
+      const user = await getRepository(User).findOne(comment.user);
+
+      comment.post = { title: post.title };
+      comment.user = { username: user.username, email: user.email };
+    }
 
     return res.json(comments);
   }
   async show(req, res) {
-    const comment = await getRepository('Comment').findOne(req.params.id);
+    const comment = await getRepository(Comment).findOne(req.params.id);
 
     if (comment) return res.json(comment);
     else return res.json({ message: 'Error, comment id not foud' });
   }
   async store(req, res) {
+    // Usuario debería venir de auth
+    // Post debería venir de params
+    const user = await getRepository(User).findOne(1);
+    const post = await getRepository(Post).findOne(1);
     const { content } = req.body;
 
     if (content) {
-      const comment = new Comment(content);
-      const commentCreated = await getRepository('Comment').save(comment);
+      let comment = new Comment(content);
+      comment.user = user;
+      comment.post = post;
+      const commentCreated = await getRepository(Comment).save(comment);
 
       return res.json(commentCreated);
     } else return res.json({ message: 'Error, faltan datos' });
   }
   async update(req, res) {
     const { content } = req.body;
-    const comment = await getRepository('Comment').findOne(req.params.id);
+    const comment = await getRepository(Comment).findOne(req.params.id);
 
     if (content && comment) {
-      const commentUpdated = getRepository('Comment').merge(comment, req.body);
+      const commentUpdated = getRepository(Comment).merge(comment, req.body);
 
-      await getRepository('Comment').manager.save(commentUpdated);
+      await getRepository(Comment).manager.save(commentUpdated);
 
       return res.json(commentUpdated);
     } else return res.json({ message: 'Error, id o contenido no ingresado' });
   }
-  async destroy(req, res) {}
+  async destroy(req, res) {
+    const comment = await getRepository(Comment).findOne(req.params.id);
+
+    if (comment) {
+      await getRepository(Comment).delete(comment);
+
+      return res.json(comment);
+    } else return res.json({ message: 'Error, no existe el comentario.' });
+  }
 
   async sendLike(req, res) {
     let comment = new Comment();
-    comment = await getRepository('Comment').findOne(req.params.id);
+    comment = await getRepository(Comment).findOne(req.params.id);
 
     comment.sendLike();
 
-    await getRepository('Comment').manager.save(comment);
+    await getRepository(Comment).manager.save(comment);
 
     return res.json({ message: comment.likes });
   }
--- app/Controllers/PostController.js
+++ app/Controllers/PostController.js
@@ -3,23 +3,47 @@ const { getRepository } = require('typeorm');
 
 // Módulos de la aplicación requeridos
 const Post = require('../Models/Post');
+const User = require('../Models/User');
+const Comment = require('../Models/Comment');
 
 class PostController {
   async index(req, res) {
-    const posts = await getRepository(Post).find();
+    let posts = await getRepository(Post).find();
+
+    for (const post of posts) {
+      const user = await getRepository(User).findOne(post.user);
+
+      // post.user = user;
+      post.user = { username: user.username, email: user.email };
+    }
 
     return res.json(posts);
   }
+
   async show(req, res) {
-    const post = await getRepository(Post).findOne(req.params.id);
+    // Obtiene el post
+    let post = await getRepository(Post).findOne(req.params.id);
+    // Obtiene el user
+    const user = await getRepository(User).findOne(post.user);
+    // Obtiene los comentarios
+    const comments = await getRepository(Comment).find({ post });
+
+    // asigna los datos en el objeto.
+    post.comments = comments;
+    post.user = user;
 
     return res.json(post);
   }
+
   async store(req, res) {
+    // Usuario debería venir de auth...
+    // si no existe debe fallar la petición
+    const user = await getRepository(User).findOne(1);
     const { title, content } = req.body;
 
     if (title && content) {
-      const post = new Post(title, content);
+      let post = new Post(title, content);
+      post.user = user;
       const postCreated = await getRepository(Post).save(post);
 
       return res.json(postCreated);
@@ -28,33 +52,37 @@ class PostController {
     }
   }
   async update(req, res) {
-    const post = await getRepository('Post').findOne(req.params.id);
+    const post = await getRepository(Post).findOne(req.params.id);
 
     if (post) {
-      const postUpdated = getRepository('Post').merge(post, req.body);
+      const postUpdated = getRepository(Post).merge(post, req.body);
 
-      await getRepository('Post').manager.save(postUpdated);
+      await getRepository(Post).manager.save(postUpdated);
 
       return res.json(postUpdated);
     } else return res.json({ message: 'Error, post not found' });
   }
+
   async destroy(req, res) {
-    const post = await getRepository('Post').findOne(req.params.id);
+    const post = await getRepository(Post).findOne(req.params.id);
 
     if (post) {
-      await getRepository('Post').delete(post.id);
-  
+      await getRepository(Post).delete(post.id);
+
       return res.json(post);
     } else return res.status(404).json({ message: 'Error, post not found' });
   }
 
   async sendStart(req, res) {
-    const post = await getRepository(Post).findOne(req.params.id);
+    let post = await getRepository(Post).findOne(req.params.id);
+    const user = await getRepository(User).findOne(post.user);
 
     if (post.starts == 0) post.starts = 1;
     else post.starts++;
 
     await getRepository(Post).manager.save(post);
+
+    post.user = { username: user.username, email: user.email };
 
     return res.json(post);
   }
